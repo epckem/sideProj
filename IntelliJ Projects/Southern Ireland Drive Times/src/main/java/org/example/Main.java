3bdcad43ba1092fd9b4f4bf6ff42f5805c96c9a6
epckem <112727855+epckem@users.noreply.github.com>
2022-11-08 16:05:40 -0500

    working on Ireland Trip HTML side project

---
+++ IntelliJ Projects/Southern Ireland Drive Times/src/main/java/org/example/Main.java
@@ -0,0 +1,81 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        double totalDriveTime = 2.8;
+        double corkToKillarney = 2.4;
+        double killarneyToDingle = 1.0;
+        double killarneyToGalway = 2.5;
+        double corkToDingle = 2.2;
+        double dingleToGalway = 3.0;
+        double galwayToCliffs = 1.1;
+        double cliffsToDublin = 3.0;
+        double cliffsToKildare = 2.4;
+        double killdareToDublin = 0.8;
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("The drive from Dublin to Cork will take 2 hours and 40 minutes");
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Where would you like to go from Cork? (1)Killarney (2)Dingle");
+        int whereToGoFromCorkScanner = Integer.parseInt(userInput.nextLine());
+        if (whereToGoFromCorkScanner == 1) {
+            totalDriveTime = totalDriveTime + corkToKillarney;
+            System.out.println("Would you like to go to (1) Dingle or (2) Galway next?");
+            int whereToGoFromKillarneyScanner = Integer.parseInt(userInput.nextLine());
+            if (whereToGoFromKillarneyScanner == 1) {
+                totalDriveTime = totalDriveTime + killarneyToDingle;
+                totalDriveTime = totalDriveTime + dingleToGalway;
+                System.out.println("The drive from Dingle to Galway will add 3 hours to your total driving time");
+                System.out.println("--------------------------------------------------------------");
+
+            }
+            if (whereToGoFromKillarneyScanner == 2) {
+                totalDriveTime = totalDriveTime + killarneyToGalway;
+            }
+            System.out.println("Would you like to stop at Kildare Village on the way to Dublin? (1)YES (2)NO");
+            System.out.println("(We are going to the Cliffs of Moher either way so I didn't make an option for that)");
+            int goToKildareVillageOrNot = Integer.parseInt(userInput.nextLine());
+            if (goToKildareVillageOrNot == 1) {
+                totalDriveTime = totalDriveTime + galwayToCliffs;
+                totalDriveTime = totalDriveTime + cliffsToKildare;
+                totalDriveTime = totalDriveTime + killdareToDublin;
+                System.out.println("You have made it back to Dublin!");
+                System.out.println("Your total driving time is: " + totalDriveTime + " hours");
+            }
+            if (goToKildareVillageOrNot == 2) {
+                totalDriveTime = totalDriveTime + galwayToCliffs;
+                totalDriveTime = totalDriveTime + cliffsToDublin;
+                System.out.println("You have made it back to Dublin!");
+                System.out.println("Your total driving time is: " + totalDriveTime + " hours");
+            }
+        }
+        if (whereToGoFromCorkScanner == 2) {
+            totalDriveTime = totalDriveTime + corkToDingle;
+            totalDriveTime = totalDriveTime + dingleToGalway;
+            System.out.println("Once you leave Dingle, the drive to Galway will add 3 hours to your total driving time");
+            System.out.println("Would you like to stop at Kildare Village on the way to Dublin? (1)YES (2)NO");
+            System.out.println("We are going to the Cliffs of Moher either way so I didn't make an option for that");
+            int secondGoToKilldareOrNot = Integer.parseInt(userInput.nextLine());
+            if (secondGoToKilldareOrNot == 1) {
+                totalDriveTime = totalDriveTime + galwayToCliffs;
+                totalDriveTime = totalDriveTime + cliffsToKildare;
+                totalDriveTime = totalDriveTime + killdareToDublin;
+                System.out.println("You have made it back to Dublin!");
+                System.out.println("Your total driving time is: " + totalDriveTime + " hours");
+            }
+            if (secondGoToKilldareOrNot == 2) {
+                totalDriveTime = totalDriveTime + galwayToCliffs;
+                totalDriveTime = totalDriveTime + cliffsToDublin;
+                System.out.println("You have made it back to Dublin!");
+                System.out.println("Your total driving time is: " + totalDriveTime + " hours");
+            }
+
+        }
+
+    }
+}
